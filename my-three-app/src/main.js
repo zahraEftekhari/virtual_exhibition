3aac7e49c726d6baf809e8232eadb5ac0e0ed133
z.Eftekhari <kavehahangar0ez@gmail.com>
2021-08-26 04:29:30 +0430

    Add the second figure

--- my-three-app/src/main.js
+++ my-three-app/src/main.js
@@ -10,28 +10,32 @@ import {
 
 import "./style.css"; // Import the stylesheet for webpack
 
-let camera, scene, renderer, mesh;
+let camera, camera2, scene, renderer, mesh, mesh2;
 
 init();
+init2();
 
 function init() {
   // camera
   camera = new PerspectiveCamera(
     70,
     window.innerWidth / window.innerHeight,
-    1,
+    100,
     1000
   );
-  camera.position.z = 400;
+  camera.position.z = 500;
+  camera.position.y = 100;
+  camera.position.x = 100;
 
   // Create scene
   scene = new Scene();
 
   // Cube
-  const geometry = new BoxBufferGeometry(200, 200, 200);
+  const geometry = new BoxBufferGeometry(100, 100, 100);
   const material = new MeshLambertMaterial({ color: 0xff0000 });
   mesh = new Mesh(geometry, material);
   scene.add(mesh);
+
 
   // Lights
   const directionalLight = new DirectionalLight(0xffffff, 0.5);
@@ -43,12 +47,54 @@ function init() {
     antialias: true,
     canvas: document.getElementById("canvas"),
   });
+
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   window.addEventListener("resize", onWindowResize, false);
 
   animate();
+}
+
+function init2() {
+  // camera
+  camera2 = new PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      100,
+      1000
+  );
+  camera2.position.z = 500;
+  camera2.position.y = 200;
+  camera2.position.x = 200;
+
+  // Create scene
+  scene = new Scene();
+
+  // Cube
+  const geometry2 = new BoxBufferGeometry(100, 100, 100);
+  const material2 = new MeshLambertMaterial({ color: 0xff0000 });
+  mesh2 = new Mesh(geometry2, material2);
+  scene.add(mesh);
+
+
+  // Lights
+  const directionalLight2 = new DirectionalLight(0xffffff, 0.5);
+  directionalLight2.position.set(0, 1, 1);
+  scene.add(directionalLight2);
+
+  // Renderer
+  renderer = new WebGLRenderer({
+    antialias: true,
+    canvas: document.getElementById("canvas"),
+  });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  window.addEventListener("resize", onWindowResize2, false);
+
+  animate2();
 }
 
 function onWindowResize() {
@@ -58,11 +104,27 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function onWindowResize2() {
+  camera2.aspect = window.innerWidth / window.innerHeight;
+  camera2.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 
   // Auto rotate cube
-  mesh.rotation.x += 0.005;
+  mesh.rotation.x += 0;
+  mesh.rotation.y += 0.01;
+}
+
+function animate2() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera2);
+
+  // Auto rotate cube
+  mesh.rotation.x += 0;
   mesh.rotation.y += 0.01;
 }
